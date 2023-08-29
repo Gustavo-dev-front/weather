e4869229fc024704ec0b6fcd89efee2553d87709
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2023-08-29 19:30:36 -0300

    Clima dos próximos 5 dias concluído

--- js/script.js
+++ js/script.js
@@ -1,26 +1,45 @@
 const INPUT = document.querySelector("#city-input");
 const SEARCH_BTN = document.querySelector(".search-btn");
 const API_KEY = `e6e698a4930e5466790effba84db0728`;
+const FORECAST_LIST = document.querySelector(".day-forecast .list");
+
+console.log(FORECAST_LIST);
+
+function createForecastCard(object) {
+  const date = object.dt_txt.split(" ")[0];
+  const icon = object.weather[0].icon;
+  const temp = object.main.temp;
+  const wind = object.wind.speed;
+  const humidity = object.main.humidity;
+  const card = `<li class="card">
+                  <span class="date">(${date})</span>
+                  <img src="https://openweathermap.org/img/wn//${icon}@4x.png" alt="weather-image">
+                  <p class="weather-info temparature">Temp: <span>${temp}°C</span></p>
+                  <p class="weather-info wind">Vento: <span>${wind} M/S</span></p>
+                  <p class="weather-info humidity">Umidade: <span>${humidity}%</span></p>
+                </li>`;
+  FORECAST_LIST.innerHTML += card;
+}
 
 async function getWeatherDetails(coordinates) {
+  FORECAST_LIST.innerHTML = "";
   const { name, lat, lon } = coordinates;
-  console.log(name, lat, lon);
-  const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&lang=pt_br&appid=${API_KEY}`;
 
   const uniqueDayForecast = [];
 
   try {
     const data = await (await fetch(WEATHER_API_URL)).json();
+    console.log(data);
     const forecastArray = data.list.filter((weatherObj) => {
       const date = weatherObj.dt_txt.split(" ")[0];
       if (!uniqueDayForecast.includes(date)) {
         uniqueDayForecast.push(date);
+        createForecastCard(weatherObj);
         return weatherObj;
       }
     });
-
     console.log(forecastArray);
-    // Continuar apartir daqui, os dados do clima estão sendo retornados, sendo necessário agora o uso do DOM para mostra-los.
   } catch (error) {
     alert("Um erro ocorreu ao requisitar o clima.");
   }
